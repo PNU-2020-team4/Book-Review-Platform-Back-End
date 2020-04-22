66e7244496447060cb02d4fac88138ddb6555d98
Kwanwoo Kim <sam@vinple.com>
2020-04-22 21:34:06 +0900

    ReviewDAO class has been added

---
+++ src/main/java/com/team4/bookreview/dao/ReviewDao.java
@@ -0,0 +1,5 @@
+package com.team4.bookreview.dao;
+
+public interface ReviewDao {
+	
+}
---
+++ src/main/java/com/team4/bookreview/vo/ReviewVO.java
@@ -0,0 +1,9 @@
+package com.team4.bookreview.vo;
+
+public class ReviewVO {
+	int idx;
+	String writer;
+	String content;
+	int start;
+	int book;
+}
