d0753436b60a0c3d6bb9e8fe78bb000c26950ca9
Kwanwoo Kim <sam@vinple.com>
2020-04-22 21:38:58 +0900

    HistoryVO has been added

---
+++ src/main/java/com/team4/bookreview/vo/HistoryVO.java
@@ -0,0 +1,15 @@
+package com.team4.bookreview.vo;
+
+import java.util.Date;
+
+public class HistoryVO {
+	// time 
+	Date date;
+	// name of book
+	String book;
+	// author
+	String book_author;
+	// user
+	int user;
+	int hist_no;
+}
