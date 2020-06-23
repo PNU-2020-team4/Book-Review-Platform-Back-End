96356a4b8b5670556fff888e45498717b31d4fc5
김관우 <rlarhksdn777@naver.com>
2020-06-23 19:15:18 +0900

    BookController - 변수명 수정

--- src/main/java/com/team4/bookreview/controller/BookController.java
+++ src/main/java/com/team4/bookreview/controller/BookController.java
@@ -22,36 +22,36 @@ public class BookController {
 	@ResponseBody
 	public String bookInsert(@RequestParam String data)  {
         logger.info("=========== [/book/insert] request ==========");
-		String JSONValue = renderer.getInsertRes(data);
-		logger.info(JSONValue);
-		return JSONValue;
+		String jsonValue = renderer.getInsertRes(data);
+		logger.info(jsonValue);
+		return jsonValue;
 	}
 	
 	@RequestMapping(value="/book/get", method = RequestMethod.GET, produces = "application/json; charset=utf8")
 	@ResponseBody
 	public String reviewSelect() {
         logger.info("=========== [/book/get] request ==========");
-        String JSONValue = renderer.getSelectRes("");
-        logger.info("Return : " + JSONValue);
-        return JSONValue;
+        String jsonValue = renderer.getSelectRes("");
+        logger.info("Return : " + jsonValue);
+        return jsonValue;
 	}
 	
 	@RequestMapping(value="/book/getone", method = RequestMethod.POST, produces = "application/json; charset=utf8")
 	@ResponseBody
 	public String reviewSelect(@RequestParam String data)  {
         logger.info("=========== [/book/getone] request ==========");
-		String JSONValue = renderer.getSelectRes(data);
-		logger.info(JSONValue);
-		return JSONValue;
+		String jsonValue = renderer.getSelectRes(data);
+		logger.info(jsonValue);
+		return jsonValue;
 	}
 	
 	@RequestMapping(value="/book/delete", method = RequestMethod.POST, produces = "application/json; charset=utf8")
 	@ResponseBody
 	public String reviewDelete(@RequestParam String data)  {
         logger.info("=========== [/book/delete] request ==========");
-		String JSONValue = renderer.getDeleteRes(data);
-		logger.info(JSONValue);
-		return JSONValue;
+		String jsonValue = renderer.getDeleteRes(data);
+		logger.info(jsonValue);
+		return jsonValue;
 		
 	}
 
@@ -59,52 +59,52 @@ public class BookController {
 	@ResponseBody
 	public String reviewUpdate(@RequestParam String data)  {
         logger.info("=========== [/book/update] request ==========");
-		String JSONValue = renderer.getUpdateRes(data);
-		logger.info(JSONValue);
-		return JSONValue;
+		String jsonValue = renderer.getUpdateRes(data);
+		logger.info(jsonValue);
+		return jsonValue;
 	}
 
 	@RequestMapping(value="/book/search/author", method = RequestMethod.POST, produces = "application/json; charset=utf8")
 	@ResponseBody
 	public String searchByAuthor(@RequestParam String data)  {
         logger.info("=========== [/book/search/author] request ==========");
-		String JSONValue = renderer.getSearchByAuthorRes(data);
-		logger.info(JSONValue);
-		return JSONValue;
+		String jsonValue = renderer.getSearchByAuthorRes(data);
+		logger.info(jsonValue);
+		return jsonValue;
 	}
 	
 	@RequestMapping(value="/book/search/name", method = RequestMethod.POST, produces = "application/json; charset=utf8")
 	@ResponseBody
 	public String searchByBookName(@RequestParam String data)  {
         logger.info("=========== [/book/search/name] request ==========");
-		String JSONValue = renderer.getSearchByNameRes(data);
-		logger.info(JSONValue);
-		return JSONValue;
+		String jsonValue = renderer.getSearchByNameRes(data);
+		logger.info(jsonValue);
+		return jsonValue;
 	}
 	@RequestMapping(value="/recommend/user/review", method = RequestMethod.POST, produces = "application/json; charset=utf8")
 	@ResponseBody
 	public String getUserReviewBasedRecommend(@RequestParam String data)  {
         logger.info("=========== [/recommend/user/review] request ==========");
-		String JSONValue = renderer.getSearchByUserReview(data);
-		logger.info(JSONValue);
-		return JSONValue;
+		String jsonValue = renderer.getSearchByUserReview(data);
+		logger.info(jsonValue);
+		return jsonValue;
 	}
 	
 	@RequestMapping(value="/recommend/user/history", method = RequestMethod.POST, produces = "application/json; charset=utf8")
 	@ResponseBody
 	public String getUserHistoryBasedRecommend(@RequestParam String data)  {
         logger.info("=========== [/recommend/user/history] request ==========");
-		String JSONValue = renderer.getSearchByUserHistory(data);
-		logger.info(JSONValue);
-		return JSONValue;
+		String jsonValue = renderer.getSearchByUserHistory(data);
+		logger.info(jsonValue);
+		return jsonValue;
 	}
 	
 	@RequestMapping(value="/recommend/database", method = RequestMethod.POST, produces = "application/json; charset=utf8")
 	@ResponseBody
 	public String getDataBasedRecommend(@RequestParam String data) {
         logger.info("=========== [/recommend/database] request ==========");
-		String JSONValue = renderer.getSearchByData(data);
-		logger.info(JSONValue);
-		return JSONValue;
+		String jsonValue = renderer.getSearchByData(data);
+		logger.info(jsonValue);
+		return jsonValue;
 	}
 }
