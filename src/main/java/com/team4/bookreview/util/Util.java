3fdc1c04ca267f595916b6a52aa5690afac8ee53
김관우 <rlarhksdn777@naver.com>
2020-06-23 20:32:08 +0900

    생성자 추가

--- src/main/java/com/team4/bookreview/util/Util.java
+++ src/main/java/com/team4/bookreview/util/Util.java
@@ -1,6 +1,9 @@
 package com.team4.bookreview.util;
 
 public class Util {
+    private Util() {
+        throw new IllegalStateException("Utility class");
+    }
     public static String toSearchString(String s) {
         s = s.replace(' ', '%');
 		s = "%" + s + "%";
